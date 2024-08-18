b3d73e20517597d9212dc5b37cf30ecb2f8823fd
Omar <omar.fayyaz@ryerson.ca>
2024-08-18 12:12:56 -0400

    trying some formulas to wram from z to a

--- cipher.rb
+++ cipher.rb
@@ -1,7 +1,30 @@
+require 'pry-byebug'
+
 def cipher(string,shift)
   arr = string.split("")
-  arr.map! do |char| 
-    char = char.ord + shift 
-  end
-  arr.map! {|char| char.chr}
+  shift = (shift % 26)
+  lower = ("a"..."z").to_a
+  upper = ("A"..."Z").to_a
+  arr.map! do |letter| 
+    if lower.include?(letter)
+      if letter.ord + shift > 122
+        shift = shift - 26
+      end
+      letter = mover(letter, shift)
+    elsif upper.include?(letter)
+      if letter.ord + shift > 90
+        shift = shift - 26
+      end
+      letter = mover(letter, shift)
+    else
+      letter = letter 
+      binding.pry
+    end
+  end.join
+ # arr.map! {|letter| letter.chr}.join
 end 
+
+def mover(string, shift)
+  string = (string.ord + shift).chr
+
+end
